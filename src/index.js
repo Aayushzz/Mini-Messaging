7932c458e177386cdbdc886283c8286970bdeb6b
chandip <maskeyc@gmail.com>
2023-12-11 12:49:23 -0500

    fixed path

--- src/index.js
+++
@@ -1,8 +0,0 @@
-onst app = require('./routes');
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
-});
